00572e22d0247ad1393daf86e47f06c8b8605540
kawan <kawammemdes123@gmail.com>
2024-10-15 15:52:39 -0300

    Estrutua-Basica

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # imobiliaria
+# Projeto 3 - Site Imobiliária
+
+Projeto de site de uma imobiliária utilizando HTML, CSS (Flex e Grid), JavaScript, Fetch, JSON, LocalStorage...
---
+++ js/app.js
@@ -0,0 +1,24 @@
+
+// Simula um JSON online (Banco de Dados)
+const listaImoveis = [
+    {
+        id:1,
+        titulo: 'Apart. 3 dorm. Papicu',
+        descricao: 'Lindo apartamento de 3 domitórios localizados num dos melhores bairros de Fortaleza.',
+        valor: 'R$ 500.000',
+        area:'90 m²',
+        quartos: 3,
+        tipo: 'apartamento', //apart, casa, terreno
+        localizacao:'Santos Dumont, Papicu',
+        venda_aluguel: 'venda', // aluguel
+        finalidade: ' residencial',//comercial
+        fotos:[
+            'link1', // padrão [0]
+            'link2',
+            'link3',
+            'link4',
+            'link5',
+        ] 
+        
+    },
+];
